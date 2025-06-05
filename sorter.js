c8b92bdf06698edcc8006473dddb14c059a3ed87
Pasha <pashafierera@gmail.com>
2025-06-05 23:00:27 +0700

    Update sorter.js

--- sorter.js
+++ sorter.js
@@ -1,18 +1,18 @@
-// ⬆️ Supabase client setup
+// Supabase client setup
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://bjsfuzlsunepfbezniey.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqc2Z1emxzdW5lcGZiZXpuaWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxMzYwMzMsImV4cCI6MjA2NDcxMjAzM30.-kjszBCPdEJHiFcFTQqLh0TUgmSj3Jo4oOb0rH336vI';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let pairings = [];
-let currentIndex = 0;
-let results = {};
-
-// ✅ URL Github
+// URL SUPERBASE
 const params = new URLSearchParams(window.location.search);
 localStorage.setItem("username", params.get("u") || "anonymous");
 localStorage.setItem("submission_link", params.get("s") || "");
+
+let pairings = [];
+let currentIndex = 0;
+let results = {};
 
 async function loadPairings() {
   const res = await fetch("pairings.json");
@@ -45,7 +45,6 @@ async function showNextPair() {
     const username = localStorage.getItem("username") || "anonymous";
     const submission_link = localStorage.getItem("submission_link") || "";
 
-    // ✅  Supabase
     await supabase.from('result').insert([
       {
         username,
@@ -61,6 +60,14 @@ async function showNextPair() {
   }
 
   const [leftPair, rightPair] = pairings[currentIndex];
+
+  // ✅ Picture fixed
+  if (!leftPair?.file || !rightPair?.file) {
+    console.error("Missing image file for:", leftPair, rightPair);
+    currentIndex++;
+    showNextPair();
+    return;
+  }
 
   const left = document.getElementById("left-img");
   const right = document.getElementById("right-img");
